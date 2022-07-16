2265930d2220bf3a0f8f327c3e093f32e086a453
inaridiy <inainainariinainari@gmail.com>
2022-07-16 23:15:58 +0900

    add welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div className="bg-base-200 container h-12">Hello</div>;
+  return (
+    <div className="bg-base-200 flex h-screen w-full items-center justify-center px-4">
+      <div className="card card-body bg-base-100 shadow-lg">
+        <h1 className="text-xl font-bold">Welcome Web2 Template.</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
